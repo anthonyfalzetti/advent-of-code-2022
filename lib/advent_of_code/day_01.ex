79a5f0b75f10120e7d3db96a5394279d1d82b2af
Anthony Falzetti <anthony.falzetti@gmail.com>
2022-12-02 15:04:33 -0700

    day 1

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,27 @@
 defmodule AdventOfCode.Day01 do
-  def part1(_args) do
+  def part1(args) do
+    args
+    |> separate_into_lists()
+    |> List.last()
   end
 
-  def part2(_args) do
+  def part2(args) do
+    args
+    |> separate_into_lists()
+    |> Enum.take(-3)
+    |> Enum.sum()
+  end
+
+  defp separate_into_lists(args) do
+    args
+    |> String.split("\n")
+    |> Enum.reduce([[]], fn
+      "", acc -> [[] | acc]
+      elem, [head | tail] ->
+        [[String.to_integer(elem) | head] | tail]
+    end)
+    |> Enum.reject(&(Enum.empty?(&1)))
+    |> Enum.map(&(Enum.sum(&1)))
+    |> Enum.sort()
   end
 end
