a0dbc92390e492b5cb67e3889ef33d48d8acd7a4
Anthony Falzetti <anthony.falzetti@gmail.com>
2022-12-06 04:52:02 -0700

    Day 06
    Not performant at all.
    ToDo:
    * Refactor

--- lib/advent_of_code/day_06.ex
+++ lib/advent_of_code/day_06.ex
@@ -1,7 +1,26 @@
 defmodule AdventOfCode.Day06 do
-  def part1(_args) do
+  def part1(input) do
+    input
+    |> String.graphemes()
+    |> Enum.chunk_every(4, 1, :discard)
+    |> find_index(4)
   end
 
-  def part2(_args) do
+  def part2(input) do
+    input
+    |> String.graphemes()
+    |> Enum.chunk_every(14, 1, :discard)
+    |> find_index(14)
+  end
+
+  defp find_index(list, count) do
+    inx = list
+    |> Enum.find_index(fn(list) ->
+      list
+      |> MapSet.new()
+      |> Enum.count() == count
+    end)
+
+    inx + count
   end
 end
